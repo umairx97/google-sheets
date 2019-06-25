764d3a660ad2b81b3a24b7e277bd24824e8a4999
Asad Ikhlas <asadikhlas03@gmail.com>
2019-06-26 02:28:27 +0500

    all done

--- server.js
+++ server.js
@@ -1,51 +1,40 @@
-const express = require('express'); 
-const bodyParser = require('body-parser'); 
-const cors = require('cors')
-const GoogleSpreadsheet = require('google-spreadsheet');
-const { promisify } = require('util');
-const creds = require('./client_secret.json')
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const GoogleSpreadsheet = require("google-spreadsheet");
+const { promisify } = require("util");
+const creds = require("./client_secret.json");
 
 const app = express();
 
-
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true})); 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use("*", cors());
 
+app.get("/data", async (req, res) => {
+  const doc = new GoogleSpreadsheet(
+    "1gK-PGhklNW_j6BbnQ_fLKaCP7ByLZk6p14wdjhXxWAQ"
+  );
+try{
+  await promisify(doc.useServiceAccountAuth)(creds);
 
-app.get('/data', async (req,res) => { 
+  const info = await promisify(doc.getInfo)();
+  const sheet = info.worksheets[0];
 
-
-        const doc = new GoogleSpreadsheet('1gK-PGhklNW_j6BbnQ_fLKaCP7ByLZk6p14wdjhXxWAQ')
-    
-        
-        await promisify(doc.useServiceAccountAuth)(creds);
-    
-        const info = await promisify(doc.getInfo)();
-        const sheet = info.worksheets[0];
-    
-        const rows = await promisify(sheet.getRows)({
-            offset: 1
-        })
-    
-        // rows.forEach(row => {
-        //     // printStudent(row)
-        //     res.send(row)
-        // });
-
-        rows.forEach(element => {
-            console.log(element)
-        });
-        res.send(rows);
-
-        
-
-})
+  const rows = await promisify(sheet.getRows)({
+    offset: 1
+  });
 
 
+  rows.forEach(element => {
+    console.log(element);
+  });
+  res.send(rows);
+}catch(error){
+    console.log('something went wrong')
+}
+});
 
-
-
-app.listen(4000, () => { 
-    console.log(`The server is running on port 4000`)
-})
+app.listen(4000, () => {
+  console.log(`The server is running on port 4000`);
+});
