003ad9ed72efcb715517510df080aab955929724
umairx97 <umair.bajwa@hotmail.com>
2019-06-26 18:26:28 +0500

    Fixed Code

--- server.js
+++ server.js
@@ -12,29 +12,27 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use("*", cors());
 
 app.get("/data", async (req, res) => {
-  const doc = new GoogleSpreadsheet(
-    "1gK-PGhklNW_j6BbnQ_fLKaCP7ByLZk6p14wdjhXxWAQ"
-  );
-try{
-  await promisify(doc.useServiceAccountAuth)(creds);
+    const doc = new GoogleSpreadsheet("1gK-PGhklNW_j6BbnQ_fLKaCP7ByLZk6p14wdjhXxWAQ");
+    try {
+        await promisify(doc.useServiceAccountAuth)(creds);
 
-  const info = await promisify(doc.getInfo)();
-  const sheet = info.worksheets[0];
+        const info = await promisify(doc.getInfo)();
+        const sheet = info.worksheets[0];
 
-  const rows = await promisify(sheet.getRows)({
-    offset: 1
-  });
+        const rows = await promisify(sheet.getRows)({
+            offset: 1
+        });
 
 
-  rows.forEach(element => {
-    console.log(element);
-  });
-  res.send(rows);
-}catch(error){
-    console.log('something went wrong')
-}
+        rows.forEach(element => {
+            console.log(element);
+        });
+        res.send(rows);
+    } catch (error) {
+        console.log('something went wrong')
+    }
 });
 
 app.listen(4000, () => {
-  console.log(`The server is running on port 4000`);
+    console.log(`The server is running on port 4000`);
 });
